18e0eacc0d1ad5fbcd61ba79f56c0f054393ecc3
Sebastian Sanchez <sebastian.asm@outlook.com>
2023-02-07 09:33:57 -0300

    conexion a mongodb

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Nest - Pokedex
+
+### Comandos para el proyecto:
+
+- Instalar Nest CLI: `pnpm add -g @nestjs/cli`
+- Levantar base de datos (MongoDB): `docker-compose up -d`
+
+### Stack de tecnologías
+
+- NestJS
+- MongoDB
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { join } from 'path'
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
 import { ServeStaticModule } from '@nestjs/serve-static'
+
+import { PokemonModule } from './pokemon/pokemon.module'
 
 @Module({
   imports: [
-    // los modulos siempre van en los imports
+    // los módulos siempre van en los imports
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    PokemonModule,
   ],
 })
 export class AppModule {}
